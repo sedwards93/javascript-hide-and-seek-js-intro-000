95e5f2c3f3f321f281d5e22d33495ca72173177d
Symion Edwards <symion.edwards@gmail.com>
2020-05-15 09:58:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
 
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = parseInt(rank[i].innerHTML, 10) + n;
+}
+}
+
+function deepestChild() {
+  let x =document.querySelector("#grand-node div div div div");
+  return x;
+}
