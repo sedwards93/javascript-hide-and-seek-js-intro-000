be9725970dd7813b7fc447469bd4ee65f0148e89
Symion Edwards <symion.edwards@gmail.com>
2020-05-15 09:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
 
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = parseInt(rank[i].innerHTML, 10) + n;
+}
+}
